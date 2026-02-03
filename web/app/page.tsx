c0bcc0e8c55c48a68daf0c5e742f6b1933de95da
yarz-dev <zyarzue.dev@gmail.com>
2026-02-03 14:08:26 +0000

    Update page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Ready To Start InstantShare - A file sharing web Application
-          </h1>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+			<main className='flex w-full max-w-3xl flex-col items-center justify-center px-8 text-center bg-white dark:bg-black'>
+				<h1 className='max-w-2xl text-4xl font-semibold leading-tight tracking-tight text-black dark:text-zinc-50'>
+					Ready to start InstantShare
+				</h1>
+
+				<p className='mt-4 text-base text-zinc-600 dark:text-zinc-400'>
+					A file sharing web application
+				</p>
+			</main>
+		</div>
+	);
 }
